b38301e6c11b96c8457a97a075f6d4ea9c20ceed
John Fu <fujohn2@hotmail.com>
2018-07-10 18:36:41 -0700

    added in the mainApp for the project and a controller class for the RootStage view

---
+++ DailyToDoApp/src/jf/dailytodo/address/mainApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * dailytodo project mainApp
+ * Starting point for the Javafx application and sets and manages the Stages 
+ * 
+ * @author johnfu
+ * 
+ */
+package jf.dailytodo.address;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ DailyToDoApp/src/jf/dailytodo/address/view/RootStageController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * RootStageController
+ * 
+ * provides functionality to the RootStage and connects the view to the code
+ * 
+ * @author johnfu
+ * 
+ */
+package jf.dailytodo.address.view;
+
+import javafx.fxml.FXML;
+
+public class RootStageController {
+	
+	
+	@FXML
+	private void initialize() {
+		//code for when the rootStage initializes 
+		
+	}
+
+}
